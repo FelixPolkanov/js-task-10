681b86f8162fac59a2e3e13a9bff4031349b4247
FelixPolkanov <felixpolkanov@gmail.com>
2022-11-05 20:08:19 +0200

    js-task-10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './css/styles.css';
+import  q from './fetchCountries'
 
 const DEBOUNCE_DELAY = 300;
+
+const inputForm = document.querySelector('input#search-box')
+inputForm.addEventListener('input', onInputForm)
+
+
+
+function onInputForm(evt) {
+    inputData = evt.currentTarget.value;
+    console.log(inputData)
+    // if (inputData) {
+    //    function fetchCountries() {
+    //       console.log(inputData) 
+    //    return fetch('https://restcountries.com/v3.1/name/inputData')   
+    // .then(response =>response.json())
+    // .then(console.log)
+    //     }
+    //     fetchCountries(inputData);
+ 
+    // }
+}
+
+
+
+
+
