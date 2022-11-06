2c8153aa9e0603cd75ad73035087011cd03f64fb
FelixPolkanov <felixpolkanov@gmail.com>
2022-11-06 12:42:08 +0200

    js-task-add

--- src/index.js
+++ src/index.js
@@ -1,27 +1,80 @@
 import './css/styles.css';
-import  q from './fetchCountries'
+import { fetchArticles } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
-const inputForm = document.querySelector('input#search-box')
-inputForm.addEventListener('input', onInputForm)
+ const refs = {
+  input: document.querySelector('#search-box'),
+  countrylistUl: document.querySelector('.country-list'),
+  countryInfoDiv: document.querySelector('.country-info'),
+};
 
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-
-function onInputForm(evt) {
-    inputData = evt.currentTarget.value;
-    console.log(inputData)
-    // if (inputData) {
-    //    function fetchCountries() {
-    //       console.log(inputData) 
-    //    return fetch('https://restcountries.com/v3.1/name/inputData')   
-    // .then(response =>response.json())
-    // .then(console.log)
-    //     }
-    //     fetchCountries(inputData);
- 
-    // }
+function onInput(evt) {
+  const inputValue = evt.target.value.trim();
+  if (inputValue === ' ') {
+     return;
+  }
+  fetchArticles(inputValue)
+    .then(verifyMarkup)
+    .catch(Notify.failure('Oops, there is no country with that name'))
+ refs.countrylistUl.innerHTML = ' ';
+ refs.countryInfoDiv.innerHTML = ' ';
 }
+
+
+
+function verifyMarkup(name) {
+  const NumbersOfCountries = name.length;
+
+  if (NumbersOfCountries > 2 && NumbersOfCountries < 10) {
+    addFirstMarkup(name);
+    return;
+  }
+  if (NumbersOfCountries === 1) {
+    addSecondMarkup(name);
+    return;
+  }
+  if (NumbersOfCountries > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    return;
+  }
+}
+
+function addFirstMarkup(name) {
+  const markup = name
+    .map(({ name, flags }) => {
+      return `<li>
+      <h2><img width="35" height="25" src="${flags.svg}"</img>
+      ${name.official}</h2>`;
+    })
+    .join('');
+  refs.countrylistUl.innerHTML = markup;
+}
+
+
+ function addSecondMarkup(name) {
+  const markup = name
+    .map(({ name, flags, capital, population, languages }) => {
+      return `<li>
+      <h2><img width="35" height="25" src="${flags.svg}"</img>
+      ${name.official}</h2>
+      <p>Capital: ${capital[0]}</p>
+      <p>Population: ${population}</p>
+      <p>Languages: ${Object.values(languages)
+        .toString()
+        .split(',')
+        .join(', ')}</p>
+    </li> `;
+    })
+    .join('');
+  refs.countrylistUl.innerHTML = markup;
+}
+
+
 
 
 
