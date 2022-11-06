b4da902fe424e964599435d37cc8d9951baf04e7
FelixPolkanov <106100262+FelixPolkanov@users.noreply.github.com>
2022-11-06 10:42:29 +0000

    Deploying to gh-pages from @ FelixPolkanov/js-task-10@2c8153aa9e0603cd75ad73035087011cd03f64fb 🚀

--- index.c0d1678f.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("input#search-box").addEventListener("input",(function(t){inputData=t.currentTarget.value,console.log(inputData)}));
-//# sourceMappingURL=index.c0d1678f.js.map
--- index.ef4d2dd3.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("input#search-box").addEventListener("input",(function(t){inputData=t.currentTarget.value,console.log(inputData)}));
-//# sourceMappingURL=index.ef4d2dd3.js.map
